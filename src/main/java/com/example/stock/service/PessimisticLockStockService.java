52e387580163f8f30a9e0403542e97511c7b4eb7
Anjaehyun <lvet0330@gmail.com>
2023-09-18 11:35:10 +0900

    feat : 재고시스템으로 알아보는 동시성 이슈 - NamedLock
    분산 락을 구현할때 주로 사용, 타임아웃 구현 쉬움. 데이터 삽입시에 정합성을 맞추는데 사용 가능.
     세션관리 잘해야하고, 구현방법 복잡함

--- src/main/java/com/example/stock/service/PessimisticLockStockService.java
+++ src/main/java/com/example/stock/service/PessimisticLockStockService.java
@@ -2,9 +2,9 @@ package com.example.stock.service;
 
 import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
--- src/main/java/com/example/stock/service/StockService.java
+++ src/main/java/com/example/stock/service/StockService.java
@@ -2,22 +2,22 @@ package com.example.stock.service;
 
 import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class StockService {
     private final StockRepository stockRepository;
 
-//    @Transactional
-    public synchronized void decrease(Long id, Long quantity) throws NotFoundException {
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public synchronized void decrease(Long id, Long quantity) {
         //stock 조회
         //재고를 감소시킨 뒤
         //갱신된 값을 저장
-        Stock stock = stockRepository.findById(id).orElseThrow(NotFoundException::new);
+        Stock stock = stockRepository.findById(id).orElseThrow();
         stock.decrease(quantity);
         stockRepository.saveAndFlush(stock);
 
